e990312b6122d8d258cfd3547921010cb8d1b411
Paul Formby <paulformby@Pauls-MacBook-Air.local>
2024-04-05 13:04:04 -0700

    ADD nav and home

---
+++ wild-link/src/components/TopNavigation.jsx
@@ -0,0 +1,20 @@
+import '../styles/TopNavigationBar.scss';
+
+const TopNavigation = () => {
+  
+  
+  // const { 
+  // } = props;
+
+  return (
+    <div className="top-nav-bar">
+      <span className="top-nav-bar__logo">Wild Link</span>
+      <div className="top-nav-bar__links">
+        <span className="top-nav-bar__patient-stories">Patient Stories</span>
+        <span className="top-nav-bar__about-us">About Us</span>
+        </div>
+        </div>
+  );
+};
+
+export default TopNavigation;
---
+++ wild-link/src/routes/HomeRoute.jsx
@@ -0,0 +1,16 @@
+import TopNavigation from '../components/TopNavigation';
+
+const HomeRoute = (props) => {
+  // const { 
+    
+  // } = props;
+
+  return (
+    <div className="home-route">
+      <TopNavigation />
+    </div>
+  );
+};
+
+export default HomeRoute;
+
