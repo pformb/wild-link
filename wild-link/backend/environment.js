90623a705305e7012325cb94be96764bf583278c
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-05 09:58:57 -0700

    FEAT enviro created

---
+++ wild-link/backend/environment.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+const ENV = process.env.NODE_ENV || "development";
+const PATH = path.resolve(__dirname, ".env." + ENV);
+
+require("dotenv").config({ path: PATH });
+
+module.exports = ENV;
+
+// Path: wild-link/backend/src/index.js
