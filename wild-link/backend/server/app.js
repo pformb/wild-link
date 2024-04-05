c6fd09e51f3fdf3eb6f2363eb9be73d91b29bbe2
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-05 13:35:49 -0700

    DELETE enviroment.js will implement in app.js

--- wild-link/backend/server/app.js
+++ wild-link/backend/server/app.js
@@ -7,3 +7,13 @@ const server = require("http").Server(app);
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 
+// const path = require("path");
+
+// const ENV = process.env.NODE_ENV || "development";
+// const PATH = path.resolve(__dirname, ".env." + ENV);
+
+// require("dotenv").config({ path: PATH });
+
+// module.exports = ENV;
+
+// Path: wild-link/backend/src/index.js
