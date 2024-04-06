29f3b580b891d517d632b6d4cb6ed810fca4f2bf
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-06 12:41:11 -0700

    ADD basic styling for login page

---
+++ frontend/src/Routes/LoginRoute.jsx
@@ -0,0 +1,18 @@
+
+import LoginPage from '../components/LoginPage';
+import '../styles/LoginPage.scss';
+
+const LoginRoute = (props) => { 
+
+  // const { 
+    
+  // } = props;
+
+  return (
+    <div className="login-route">
+      <LoginPage />
+      </div>
+  );
+};
+
+  export default LoginRoute;
---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,16 @@
+import  '../styles/LoginPage.scss';
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
