062c2d23f5d1aa76746b2f17120447ea4be32385
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-06 19:00:38 -0700

    BROKEN not rending

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
-
+import React from 'react';
+// import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginRoute from './routes/LoginRoute';
 
-import './App.css';
+//troubleshooting with https://www.syncfusion.com/blogs/post/react-router-vs-react-router-dom
 
 function App() {
   return (
-    <div className="App">
-      <LoginRoute />
-    </div>
+    <BrowserRouter />
+    <Routes>   
+     <div className="App">
+      {/* Route exact path="/" component={Home} */}
+      {/* route to home which we will build all other routes from */}
+      <Route path="/login" component={LoginRoute} />
+          {/* <LoginRoute /> */}
+      </div>
+  </Routes>
+  </BrowserRouter>
   );
 }
 
