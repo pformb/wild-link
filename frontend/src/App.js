3f3dffd216c0966fb48bd3324b07c5ef551b3e30
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-06 19:06:41 -0700

    odified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route} from 'react-router-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import LoginRoute from './routes/LoginRoute';
 
-//troubleshooting with https://www.syncfusion.com/blogs/post/react-router-vs-react-router-dom
-
 function App() {
   return (
-    <BrowserRouter />
-    <Routes>   
+    <Router>   
      <div className="App">
       {/* Route exact path="/" component={Home} */}
       {/* route to home which we will build all other routes from */}
       <Route path="/login" component={LoginRoute} />
           {/* <LoginRoute /> */}
       </div>
-  </Routes>
-  </BrowserRouter>
+  </Router>
   );
 }
 
