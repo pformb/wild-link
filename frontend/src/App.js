dea14b2a692ec3e5a3477ac524ca02e1ba5e5694
Paul Formby <paulformby@Pauls-MacBook-Air.local>
2024-04-07 19:10:13 -0700

    UPDATE app.js and add photo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 
+import HomeRoute from './routes/HomeRoute';
 import './App.css';
-import LoginRoute from './routes/LoginRoute';
-import RegisterRoute from './routes/RegisterRoute';
+
 function App() {
   return (
     <div className="App">
-      <LoginRoute />
-      <RegisterRoute />
+      <HomeRoute />
     </div>
   );
 }
