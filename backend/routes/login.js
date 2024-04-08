3682bbc3d68d121bb0e48c6732f56cc23051fd84
David Carmichael <carmichael.db88@gmail.com>
2024-04-08 15:32:17 -0700

    Add: logout request to clear cookie

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -10,14 +10,19 @@ module.exports = db => {
     }
     if (account && (await bcrypt.compare(req.body.password, account.password))) {
       req.session.userId = account.id;
+      req.session.name = account.first_name;
       req.session.role = account.role;
-      console.log("Cookie set successfully");
       res.json({ message: "Login successful" });
     } else {
       // Account not found
       res.status(400).send("Error 400: Email or Password does not match our records, please check the email and password.");
     }
   });
-
+  
+  router.post("/logout", async (req, res) => {
+    req.session = null
+    res.json({ message: "Logout successful" });
+    console.log(req.session);
+  });
   return router;
 };
