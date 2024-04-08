aee39a00221d620896c08401ec2b98cda92656b3
Paul Formby <paulformby@Pauls-MacBook-Air.local>
2024-04-07 17:48:22 -0700

    add home page picture and write up

--- wild-link/src/components/HomePage.jsx
+++ wild-link/src/components/HomePage.jsx
@@ -7,7 +7,30 @@ const HomePage = () => {
 
   return (
     <div className="home-page">
-      <img className="home-cover" src="/home-cover-photo.jpg" alt="home" />
+      <img className="home-cover" src="/home-cover-photo.png" alt="home" />
+      <br />
+      <h1>Welcome to Wild Link</h1>
+      <br />
+      <h2>
+        Wild Link is a groundbreaking web application designed 
+        to harness the power of collective goodwill for the 
+        conservation of wildlife. Much like popular crowdfunding 
+        platforms, Wild Link provides a seamless interface for 
+        users to create and support campaigns dedicated solely to 
+        wildlife causes. Whether it's protecting endangered 
+        species, preserving vital habitats, or funding research 
+        initiatives, Wild Link empowers users to directly 
+        contribute to the well-being of our planet's diverse 
+        flora and fauna. With its intuitive design and robust 
+        features, Wild Link serves as a virtual hub where 
+        passionate individuals, organizations, and wildlife 
+        enthusiasts unite to make a tangible difference in the 
+        conservation efforts worldwide. Join the Wild Link 
+        community today and be a part of the movement to 
+        safeguard our planet's natural treasures for generations 
+        to come. 
+        </h2>
+
     </div>
   );
 };
--- wild-link/src/routes/HomeRoute.jsx
+++ wild-link/src/routes/HomeRoute.jsx
@@ -4,7 +4,6 @@ import TopNavigation from '../components/TopNavigation';
 
 const HomeRoute = (props) => {
   // const { 
-    
   // } = props;
 
   return (
