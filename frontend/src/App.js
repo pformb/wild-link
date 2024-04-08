23d7264543f9391c269690c989ee3e8e98e251d2
Paul Formby <paulformby@Pauls-MacBook-Air.local>
2024-04-08 09:07:33 -0700

    set up routes for home, login, register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
-import { useApplicationData } from '../src/hooks/useApplicationData.js';
+// import { useApplicationData } from '../src/hooks/useApplicationData.js';
 import HomeRoute from './routes/HomeRoute';
+import LoginPage from './components/LoginPage.jsx'
+import RegistrationPage from './components/RegistrationPage.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  const { 
-    returnHome,
-    fetchLogin
-  } = useApplicationData();
+  // const { 
+  //   returnHome,
+  //   fetchLogin
+  // } = useApplicationData();
 
   return (
-    <div className="App">
-      <HomeRoute 
-      returnHome={returnHome}
-      fetchLogin={fetchLogin}
-      />
+    
+    <div className="App"> 
+    <BrowserRouter>
+    <Routes>
+    <Route index element={<HomeRoute />} />
+    <Route path="/home" element={<HomeRoute />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegistrationPage />} />
+    
+    </Routes>
+    </BrowserRouter>
     </div>
+   
   );
 }
 
--- frontend/src/components/TopNavigation.jsx
+++ frontend/src/components/TopNavigation.jsx
@@ -1,18 +1,18 @@
 import '../styles/TopNavigationBar.scss';
+import { Link } from 'react-router-dom';
 
 const TopNavigation = () => {
-  
 
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">Wild Link</span>
+      <Link to="/home" className="top-nav-bar__logo">Wild Link</Link>
       <div className="top-nav-bar__links">
         <span className="top-nav-bar__patient-stories">Patient Stories</span>
         <span className="top-nav-bar__about-us">About Us</span>
         </div>
         <div className="top-nav-bar__login-register">
-          <span className="top-nav-bar__login">Login</span>
-          <span className="top-nav-bar__register">Register</span>
+        <Link to="/login" className="top-nav-bar__login">Login</Link>
+        <Link to="/register" className="top-nav-bar__register">Register</Link>
         </div>
         </div>
   );
