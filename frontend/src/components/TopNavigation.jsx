915133e043d42c68c5110f126d0dc15b407fbd8a
Paul Formby <paulformby@Pauls-MacBook-Air.local>
2024-04-07 18:45:25 -0700

    MERGE feature nav/home

---
+++ frontend/src/components/TopNavigation.jsx
@@ -0,0 +1,24 @@
+import '../styles/TopNavigationBar.scss';
+
+const TopNavigation = () => {
+  
+  
+  // const { 
+  // } = props;
+
+  return (
+    <div className="top-nav-bar">
+      <span className="top-nav-bar__logo">Wild Link</span>
+      <div className="top-nav-bar__links">
+        <span className="top-nav-bar__patient-stories">Patient Stories</span>
+        <span className="top-nav-bar__about-us">About Us</span>
+        </div>
+        <div className="top-nav-bar__login-register">
+          <span className="top-nav-bar__login">Login</span>
+          <span className="top-nav-bar__register">Register</span>
+        </div>
+        </div>
+  );
+};
+
+export default TopNavigation;
