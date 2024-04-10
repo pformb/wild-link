3064d3e1501031be860a2c450ace5a0b4c85bbd6
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-09 17:54:39 -0700

    ADD login logic

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,12 +1,48 @@
+import React, { useState } from 'react';
 import  '../styles/LoginPage.scss';
 
+//mock user data
+let users = [
+  { username: 'user1', password: 'pas$word1' },
+  { username: 'user2', password: 'password2' },
+  { username: 'user3', password: 'password3' }
+];
+
 const LoginPage = () => {
+  //manage user data in state
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  //form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let userExists = users.find(user => user.username === username && user.password === password);
+
+    if (!userExists) {
+      alert('Invalid username or password');
+      return;
+    }
+    
+    alert(`Welcome back, ${username}!`);
+  }
+
   return (
     <div className="login-page">
       <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit}>
+        <input 
+         type="text"
+         placeholder="Username"
+         value={username}
+         onChange={(e) => setUsername(e.target.value)}
+          />
+        <input
+         type="password"
+         placeholder="Password"
+         value={password}
+         onChange={(e) => setPassword(e.target.value)} 
+         />
         <button type="submit">Login</button>
       </form>
     </div>
