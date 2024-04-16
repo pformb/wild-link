2cf5bca55b04a670326bed4a3d3ebdfb275720fd
David Carmichael <carmichael.db88@gmail.com>
2024-04-16 15:21:33 -0700

    Styles: Patient Form Styles added

--- frontend/src/components/PatientForm.jsx
+++ frontend/src/components/PatientForm.jsx
@@ -1,143 +1,263 @@
 import React from "react";
 import Select from 'react-select'
 import { usePatientForm } from "../hooks/usePatientForm";
+import {TextField, Checkbox, Grid, Box, Button, Typography } from "@mui/material"
+import makeAnimated from 'react-select/animated';
 
 const PatientForm = () => {
   const { formData, handleSubmit, handleInputChange, handleCheckboxChange, handleSelectChange, handleMultiSelectChange, handleGenerateStory, isLoading, options } = usePatientForm();
+  const animatedComponents = makeAnimated();
+
+  const customSelectStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      minHeight: "56px",
+      fontFamily: "Arial, sans-serif",
+      borderColor: state.isFocused ? "#ccc" : "#ccc",
+      boxShadow: state.isFocused ? "0 0 0 1px #ccc" : "none",
+      "&:hover": {
+        borderColor: state.isFocused ? "#aaa" : "#ccc",
+      },
+      marginTop: '12px',
+    }),
+    valueContainer: (provided, state) => ({
+      ...provided,
+      padding: "18.5px 14px",
+    }),
+    input: (provided, state) => ({
+      ...provided,
+      margin: 0,
+      padding: 0,
+    }),
+    placeholder: (provided, state) => ({
+      ...provided,
+      position: "absolute",
+      top: "50%",
+      transform: "translateY(-50%)",
+      color: "#aaa",
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      color: state.isFocused ? "black" : "black",
+      overflow: "visible",
+    }),
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Case Number
-        <input
-          type="text"
-          name="patient_case"
-          value={formData.patientDetails.patient_case}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Admission Date
-        <input
-          type="date"
-          name="date_admitted"
-          value={formData.patientDetails.date_admitted}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Species
-        <Select
-          options={options.speciesOptions}
-          name="species_id"
-          value={options.speciesOptions.find(
-            (option) => option.value === formData.patientDetails.species_id
-          )}
-          onChange={handleSelectChange}
-          isSearchable
-        />
-      </label>
-      <label>
-        Find Location
-        <input
-          type="text"
-          name="location_found"
-          value={formData.patientDetails.location_found}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Released
-        <input
-          type="checkbox"
-          name="is_released"
-          checked={formData.patientDetails.is_released}
-          onChange={handleCheckboxChange}
-        />
-      </label>
-      <label>
-        Released Date
-        <input
-          type="date"
-          name="release_date"
-          value={formData.patientDetails.release_date}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Age
-        <Select
-          name="age_range_id"
-          options={options.ageRangeOptions}
-          value={options.ageRangeOptions.find(
-            (option) => option.value === formData.patientDetails.age_range_id
-          )}
-          onChange={handleSelectChange}
-        />
-      </label>
-      <label>
-        Conditions
-        <Select
-          key={formData.patientConditions.length}
-          name="patientConditions"
-          options={options.conditionOptions}
-          value={options.conditionOptions.filter((option) =>
-            formData.patientConditions.includes(option.value)
-          )}
-          onChange={handleMultiSelectChange}
-          isMulti
-          isSearchable
-        />
-      </label>
-      <label>
-        Treatments
-        <Select
-          name="patientTreatments"
-          options={options.treatmentOptions}
-          value={options.treatmentOptions.filter((option) =>
-            formData.patientTreatments.includes(option.value)
-          )}
-          onChange={handleMultiSelectChange}
-          isSearchable
-          isMulti
-        />
-      </label>
-      <label>
-        Animal Photo
-        <img
-          src={
-            formData.patientDetails.image ||
-            options.speciesOptions.find(
-              (option) => option.value === formData.patientDetails.species_id
-            )?.image ||
-            ""
-          }
-          alt="Select Species for Default"
-          style={{ width: 200, height: 200 }}
-        />
-        <input
-          type="text"
-          name="image"
-          value={formData.patientDetails.image}
-          onChange={handleInputChange}
-          placeholder="Enter image URL"
-        />
-      </label>
-      <label>
-        Patient Story
-        <button type="button" onClick={handleGenerateStory} disabled={isLoading}>
-          Generate Story
-        </button>
-        <input
-          type="text"
-          name="story"
-          value={isLoading ? "Generating Story... Please Wait" : formData.patientDetails.story}
-          disabled={isLoading}
-          onChange={handleInputChange}
-        />
-      </label>
-      <input type="submit" value="Save Patient" />
-    </form>
+    <Box sx={{ width: "80%", mx: "auto", boxShadow: 3, p: 3 }}>
+      <form onSubmit={handleSubmit}>
+        <Grid
+          container
+          spacing={2}
+          sx={{ "& > .MuiGrid-item": { padding: 2 } }}
+        >
+          <Grid item xs={12} md={6} container>
+            <Grid item xs={3}>
+              <Typography sx={{ pt: 2 }}>Case Number</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <TextField
+                fullWidth
+                type="text"
+                name="patient_case"
+                value={formData.patientDetails.patient_case}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Typography sx={{ pt: 2 }}>Admission Date</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <TextField
+                fullWidth
+                type="date"
+                name="date_admitted"
+                value={formData.patientDetails.date_admitted}
+                onChange={handleInputChange}
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6} container>
+            <Grid item xs={3}>
+              <Typography sx={{ pt: 2 }}>Find Location</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <TextField
+                fullWidth
+                type="text"
+                name="location_found"
+                value={formData.patientDetails.location_found}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Typography sx={{ pt: 2 }}>Species</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <Select
+                styles={customSelectStyles}
+                components={animatedComponents}
+                options={options.speciesOptions}
+                name="species_id"
+                value={options.speciesOptions.find(
+                  (option) =>
+                    option.value === formData.patientDetails.species_id
+                )}
+                onChange={handleSelectChange}
+                isSearchable
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6} container>
+            <Grid item xs={3}>
+              <Typography sx={{ pt: 2 }}>Age</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <Select
+                styles={customSelectStyles}
+                name="age_range_id"
+                options={options.ageRangeOptions}
+                value={options.ageRangeOptions.find(
+                  (option) =>
+                    option.value === formData.patientDetails.age_range_id
+                )}
+                components={animatedComponents}
+                onChange={handleSelectChange}
+              />
+            </Grid>
+            <Grid item xs={12} alignItems="center" container>
+              <Grid item xs={false} sm={1} />
+              <Grid
+                item
+                xs={12}
+                sm={9}
+                sx={{ display: "flex", alignItems: "center" }}
+              >
+                <Typography sx={{ mr: 1, mb: 0 }}>Released</Typography>
+                <Checkbox
+                  name="is_released"
+                  checked={formData.patientDetails.is_released}
+                  onChange={handleCheckboxChange}
+                  size="large"
+                  sx={{ paddingLeft: 4 }}
+                />
+              </Grid>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography sx={{ pt: 2 }}>Release Date</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <TextField
+                fullWidth
+                type="date"
+                name="release_date"
+                value={formData.patientDetails.release_date}
+                onChange={handleInputChange}
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Typography sx={{ pt: 2 }}>Conditions</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <Select
+                styles={customSelectStyles}
+                components={animatedComponents}
+                key={formData.patientConditions.length}
+                name="patientConditions"
+                options={options.conditionOptions}
+                value={options.conditionOptions.filter((option) =>
+                  formData.patientConditions.includes(option.value)
+                )}
+                onChange={handleMultiSelectChange}
+                isMulti
+                isSearchable
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Typography sx={{ pt: 2 }}>Treatments</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <Select
+                styles={customSelectStyles}
+                components={animatedComponents}
+                name="patientTreatments"
+                options={options.treatmentOptions}
+                value={options.treatmentOptions.filter((option) =>
+                  formData.patientTreatments.includes(option.value)
+                )}
+                onChange={handleMultiSelectChange}
+                isSearchable
+                isMulti
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography sx={{ pt: 2 }}>Patient Image</Typography>
+            <img
+              src={
+                formData.patientDetails.image ||
+                options.speciesOptions.find(
+                  (option) =>
+                    option.value === formData.patientDetails.species_id
+                )?.image ||
+                ""
+              }
+              alt="Select Species for Default"
+              style={{ width: "auto", maxHeight: 225 }}
+            />
+            <TextField
+              fullWidth
+              label="Image URL"
+              type="text"
+              name="image"
+              value={formData.patientDetails.image}
+              onChange={handleInputChange}
+              placeholder="Enter image URL"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography sx={{ pt: 2 }}>Patient Story</Typography>
+            <Button
+              type="button"
+              onClick={handleGenerateStory}
+              disabled={isLoading}
+              variant="contained"
+              color="secondary"
+            >
+              Generate Story
+            </Button>
+            <TextField
+              fullWidth
+              type="text"
+              name="story"
+              value={
+                isLoading
+                  ? "Generating Story... Please Wait"
+                  : formData.patientDetails.story
+              }
+              disabled={isLoading}
+              onChange={handleInputChange}
+              multiline
+              minRows={8}
+              variant="outlined"
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="success"
+              sx={{ mt: 2 }}
+            >
+              Save Patient
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Box>
   );
 };
 
