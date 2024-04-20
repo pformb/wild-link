e022c8ae0752ad03513eb8001af7bdbd0805e86b
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-20 12:51:50 -0700

    FIX links added back to top nav

--- frontend/src/components/TopNavigation.jsx
+++ frontend/src/components/TopNavigation.jsx
@@ -1,5 +1,4 @@
-
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import { useAuth } from "../contexts/AuthContext";
 
@@ -7,8 +6,7 @@ const TopNavigation = () => {
   const { user, logout } = useAuth();
 
   // Access roles and orgId from user
-const orgId = user?.orgId;
-
+  const orgId = user?.orgId;
 
   console.log('checking if user is logged in:');
   if (user) {
@@ -18,9 +16,19 @@ const orgId = user?.orgId;
   return (
     <AppBar position="static" style={{ backgroundColor: '#333333', boxShadow: '0 4px 6px -6px #222', height: '72px' }}>
        <Toolbar style={{ padding: 0 }}>
-        <Typography variant="h6" style={{ flexGrow: 3, fontFamily: "'Luckiest Guy', cursive", fontSize: '38px' }}>
-        <RouterLink to="/home" style={{ textDecoration: 'none', color: 'white' }}>Wild Link</RouterLink>
-        </Typography>
+       <Typography variant="h6" style={{ flexGrow: 3, fontFamily: "'Luckiest Guy', cursive", fontSize: '38px' }}>
+            <RouterLink to="/home" style={{ textDecoration: 'none', color: 'white' }}>Wild Link</RouterLink>
+          </Typography>
+
+        <Box display="flex" justifyContent="flex-start" style={{ padding: '20px' }}>
+          <RouterLink to="/patientstories" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Patient Stories</RouterLink>
+          <RouterLink to="/aboutus" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>About Us</RouterLink>
+          <RouterLink to="/contactus" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Contact Us</RouterLink>
+        
+
+        {user && <RouterLink to="/donate" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Donate</RouterLink>}
+
+        </Box>
 
         <div className="top-nav-bar__login-register">
         {user ? (
