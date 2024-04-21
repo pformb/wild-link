0e76230874a6f9d15e06b04970008ebcd5fb6307
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-21 11:48:48 -0700

    ADD header and pagination to donations table

--- frontend/src/components/DonationsTable.jsx
+++ frontend/src/components/DonationsTable.jsx
@@ -1,81 +1,58 @@
-
-
 import React from 'react';
-import { Container, Table, TableBody, TableCell, TableContainer, TableRow, Paper } from '@mui/material';
+import { Container, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Paper } from '@mui/material';
 
 const DonationsTable = ({ donation, isOrg }) => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+const handleChangePage = (event, newPage) => {
+  setPage(newPage);
+};
+
+const handleChangeRowsPerPage = (event) => { 
+  setRowsPerPage(parseInt(event.target.value, 10));
+  setPage(0);
+};
+
   return (
     <Container align="center">
       <TableContainer component={Paper} sx={{ width: '80vw', maxWidth: '100%' }}>
         <Table>
+          <TableHead>
+            <TableRow>
+              {isOrg && <TableCell>User Full Name</TableCell>}
+              <TableCell>Patient Case</TableCell>
+              <TableCell>Donation Amount</TableCell>
+              <TableCell>Donation Date</TableCell>
+              <TableCell>Image</TableCell>
+            </TableRow>
+          </TableHead>
           <TableBody>
-            {donation.map((donation, index) => (
-              isOrg ? (
-                <TableRow key={index}>
-                  <TableCell>User Full Name: {donation.user_full_name}</TableCell>
-                  <TableCell>Patient Case: {donation.patient_case}</TableCell>
-                  <TableCell>Donation Amount: ${donation.donation_in_cents / 100}.00 </TableCell>
-                  <TableCell>Donation Date: {new Date(donation.donation_date).toLocaleDateString()}</TableCell>
-                  <TableCell>
-                    <img src={donation.image} alt="User" style={{ width: '50px', height: '50px' }} />
-                  </TableCell>
-                </TableRow>
-              ) : (
-                <TableRow key={index}>
-                  <TableCell>Patient Case: {donation.patient_case}</TableCell>
-                  <TableCell>Donation Amount: ${donation.donation_in_cents / 100}.00 </TableCell>
-                  <TableCell>Donation Date: {new Date(donation.donation_date).toLocaleDateString()}</TableCell>
-                  <TableCell>
-                    <img src={donation.image} alt="User" style={{ width: '50px', height: '50px' }} />
-                  </TableCell>
-                </TableRow>
-              )
+            {donation.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((donation, index) => (
+              <TableRow key={index}>
+                {isOrg && <TableCell>{donation.user_full_name}</TableCell>}
+                <TableCell>{donation.patient_case}</TableCell>
+                <TableCell>${donation.donation_in_cents / 100}.00</TableCell>
+                <TableCell>{new Date(donation.donation_date).toLocaleDateString()}</TableCell>
+                <TableCell>
+                  <img src={donation.image} alt="User" style={{ width: '50px', height: '50px' }} />
+                </TableCell>
+              </TableRow>
             ))}
           </TableBody>
         </Table>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={donation.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </TableContainer>
     </Container>
   );
 };
 
 export default DonationsTable;
-
-// return (
-//   <Container align="center">
-//     <TableContainer component={Paper} sx={{ width: '80vw', maxWidth: '100%' }}>
-//       <Table>
-//         <TableBody>
-//           {donation.map((donation, index) => (
-//             <React.Fragment key={index}>
-//               <TableRow>
-//                 <TableCell>First Name</TableCell>
-//                 <TableCell>{donation.first_name}</TableCell>
-//               </TableRow>
-//               <TableRow>
-//                 <TableCell>Last Name</TableCell>
-//                 <TableCell>{donation.last_name}</TableCell>
-//               </TableRow>
-//               <TableRow>
-//                 <TableCell>Email</TableCell>
-//                 <TableCell>{donation.email}</TableCell>
-//               </TableRow>
-//               <TableRow>
-//                 <TableCell>Phone Number</TableCell>
-//                 <TableCell>{donation.phone_number}</TableCell>
-//               </TableRow>
-//               <TableRow>
-//                 <TableCell>Address</TableCell>
-//                 <TableCell>{donation.address}</TableCell>
-//               </TableRow>
-//               <TableRow>
-//                 <TableCell>Image</TableCell>
-//                 <TableCell><img src={donation.image} alt="User" /></TableCell>
-//               </TableRow>
-//             </React.Fragment>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   </Container>
-// );
-// };
