dba01b91f5310a2944068fad44cda2314c935e14
Paul Formby <paulformby@Pauls-MacBook-Air.local>
2024-04-20 21:28:34 -0700

    syle cards

--- frontend/src/components/OrganizationCard.jsx
+++ frontend/src/components/OrganizationCard.jsx
@@ -1,24 +1,42 @@
 import '../styles/OrganizationCard.scss';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
 
 const OrganizationCard = (orgData) => {
 
   return (
-    <div className="organization-card">
-      <img 
-        className="organization-prof-pic" 
-        src={orgData.image} 
-        alt={orgData.name} 
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={orgData.image}
+        title={orgData.name} 
       />
-      <h2 className="organization-name">{orgData.organization_name}</h2> 
-      <p className="organization-location">{orgData.location}</p> 
-      <p className="organization-website">{orgData.website_url}</p> 
-      <p className="organization-phone">{orgData.phone_number}</p> 
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        <h2 className="organization-name">{orgData.organization_name}</h2>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        <p className="organization-location">{orgData.location}</p>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        <p className="organization-location">{orgData.website_url}</p>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        <p className="organization-location">{orgData.phone_number}</p>
+        </Typography>
+        
+      </CardContent>
+      <CardActions>
       <Link to={`/${orgData.id}/patients`}>
-      <Button variant="outlined">View More</Button>
+      <Button variant="outlined">View</Button>
       </Link>
-    </div>
+      </CardActions>
+    </Card>
   );
 }
 
