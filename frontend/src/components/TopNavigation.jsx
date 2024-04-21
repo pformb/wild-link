4dbe4d4e323b408f42a97d80d000096aafd776fd
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-20 20:30:55 -0700

    FIX grid styling hompage, FIX: link styling top nav

--- frontend/src/components/TopNavigation.jsx
+++ frontend/src/components/TopNavigation.jsx
@@ -14,40 +14,41 @@ const TopNavigation = () => {
   }
   
   return (
-<AppBar position="static" style={{ 
-  background: 'linear-gradient(90deg, rgba(245, 222, 179, 1) 0%, rgba(33,94,33,1) 100%)', 
-  boxShadow: '0 4px 6px -6px #222', 
-  height: '72px' 
-}}>
-       <Toolbar style={{ padding: 0 }}>
-       <Typography variant="h6" style={{ flexGrow: 3, fontFamily: "'Luckiest Guy', cursive", fontSize: '38px' }}>
-       <RouterLink to="/home" style={{ textDecoration: 'none', color: 'white' }}>Home </RouterLink>
+    <AppBar position="static" style={{ 
+      background: 'linear-gradient(90deg, rgba(245, 222, 179, 1) 0%, rgba(33,94,33,1) 100%)', 
+      boxShadow: '0 4px 6px -6px #222', 
+      height: '72px' 
+    }}>
+      <Toolbar style={{ padding: 0 }}>
+        <Box display="flex" justifyContent="flex-start" style={{ flexGrow: 3 }}>
+          <Typography variant="h6" style={{ fontFamily: "'Luckiest Guy', cursive", fontSize: '38px' }}>
+            <RouterLink to="/home" style={{ textDecoration: 'none', color: 'white', paddingLeft: '10px' }}>Home </RouterLink>
           </Typography>
+        </Box>
 
         <Box display="flex" justifyContent="flex-start" style={{ padding: '20px' }}>
           <RouterLink to="/patientstories" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Patient Stories</RouterLink>
-        {user && <RouterLink to="/donate" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Donate</RouterLink>}
-
+          {user && <RouterLink to="/donate" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Donate</RouterLink>}
         </Box>
 
-        <div className="top-nav-bar__login-register">
-        {user ? (
-  <>
-    <span className="top-nav-bar__login-message">Hello, {user.first_name}!</span>
-    {user.orgId ? (
-      <RouterLink to={`/organizations/${orgId}/profile`} className="top-nav-bar__profile" style={{ textDecoration: 'none', color: 'white', padding: '20px' }}>View Profile</RouterLink>
-    ) : (
-      <RouterLink to={`/users/${user.userId}`} className="top-nav-bar__profile" style={{ textDecoration: 'none', color: 'white', padding: '20px' }}>View Profile</RouterLink>
-    )}
-    <button onClick={logout} className="top-nav-bar__logout">Logout</button>
-  </>
-) : (
-  <>
-     <RouterLink to="/login" className="top-nav-bar__login" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Login</RouterLink>
-    <RouterLink to="/register" className="top-nav-bar__register" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Register</RouterLink>
-  </>
-)}
-      </div>
+        <div className="top-nav-bar__login-register" style={{ paddingRight: '10px'}}>
+          {user ? (
+            <>
+              <span className="top-nav-bar__login-message">Hello, {user.first_name}!</span>
+              {user.orgId ? (
+                <RouterLink to={`/organizations/${orgId}/profile`} className="top-nav-bar__profile" style={{ textDecoration: 'none', color: 'white', padding: '20px' }}>View Profile</RouterLink>
+              ) : (
+                <RouterLink to={`/users/${user.userId}`} className="top-nav-bar__profile" style={{ textDecoration: 'none', color: 'white', padding: '20px' }}>View Profile</RouterLink>
+              )}
+              <button onClick={logout} className="top-nav-bar__logout">Logout</button>
+            </>
+          ) : (
+            <>
+              <RouterLink to="/login" className="top-nav-bar__login" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Login</RouterLink>
+              <RouterLink to="/register" className="top-nav-bar__register" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Register</RouterLink>
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
