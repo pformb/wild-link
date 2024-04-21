bcddb3d490dc6675f9b2774bad6642c36ad0ed94
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-20 20:54:35 -0700

    EDIT: font to match site, nav now sticky

--- frontend/src/components/TopNavigation.jsx
+++ frontend/src/components/TopNavigation.jsx
@@ -12,40 +12,40 @@ const TopNavigation = () => {
   if (user) {
     console.log(`Creating link to user profile. usersId: ${user.userId}`);
   }
-  
+
   return (
-    <AppBar position="static" style={{ 
-      background: 'linear-gradient(90deg, rgba(245, 222, 179, 1) 0%, rgba(33,94,33,1) 100%)', 
-      boxShadow: '0 4px 6px -6px #222', 
-      height: '72px' 
+    <AppBar position="sticky" style={{
+      background: 'linear-gradient(90deg, rgba(245, 222, 179, 1) 0%, rgba(33,94,33,1) 100%)',
+      boxShadow: '0 4px 6px -6px #222',
+      height: '72px'
     }}>
       <Toolbar style={{ padding: 0 }}>
         <Box display="flex" justifyContent="flex-start" style={{ flexGrow: 3 }}>
-          <Typography variant="h6" style={{ fontFamily: "'Luckiest Guy', cursive", fontSize: '38px' }}>
-            <RouterLink to="/home" style={{ textDecoration: 'none', color: 'white', paddingLeft: '10px' }}>Home </RouterLink>
+          <Typography variant="h6" style={{ fontFamily: "'Zilla Slab', Arial, sans-serif", fontSize: '38px' }}>
+            <RouterLink to="/home" style={{ textDecoration: 'none', color: 'white', fontFamily: "'Zilla Slab', Arial, sans-serif", paddingLeft: '10px' }}>Home </RouterLink>
           </Typography>
         </Box>
 
         <Box display="flex" justifyContent="flex-start" style={{ padding: '20px' }}>
-          <RouterLink to="/patientstories" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Patient Stories</RouterLink>
-          {user && <RouterLink to="/donate" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Donate</RouterLink>}
+          <RouterLink to="/patientstories" style={{ textDecoration: 'none', color: 'white', padding: '5px', fontFamily: "'Zilla Slab', Arial, sans-serif" }}>Patient Stories</RouterLink>
+          {user && <RouterLink to="/donate" style={{ textDecoration: 'none', color: 'white', padding: '5px', fontFamily: "'Zilla Slab', Arial, sans-serif" }}>Donate</RouterLink>}
         </Box>
 
-        <div className="top-nav-bar__login-register" style={{ paddingRight: '10px'}}>
+        <div className="top-nav-bar__login-register" style={{ paddingRight: '10px' }}>
           {user ? (
             <>
               <span className="top-nav-bar__login-message">Hello, {user.first_name}!</span>
               {user.orgId ? (
-                <RouterLink to={`/organizations/${orgId}/profile`} className="top-nav-bar__profile" style={{ textDecoration: 'none', color: 'white', padding: '20px' }}>View Profile</RouterLink>
+                <RouterLink to={`/organizations/${orgId}/profile`} className="top-nav-bar__profile" style={{ textDecoration: 'none', color: 'white', padding: '20px', fontFamily: "'Zilla Slab', Arial, sans-serif" }}>View Profile</RouterLink>
               ) : (
-                <RouterLink to={`/users/${user.userId}`} className="top-nav-bar__profile" style={{ textDecoration: 'none', color: 'white', padding: '20px' }}>View Profile</RouterLink>
+                <RouterLink to={`/users/${user.userId}`} className="top-nav-bar__profile" style={{ textDecoration: 'none', color: 'white', padding: '20px', fontFamily: "'Zilla Slab', Arial, sans-serif" }}>View Profile</RouterLink>
               )}
               <button onClick={logout} className="top-nav-bar__logout">Logout</button>
             </>
           ) : (
             <>
-              <RouterLink to="/login" className="top-nav-bar__login" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Login</RouterLink>
-              <RouterLink to="/register" className="top-nav-bar__register" style={{ textDecoration: 'none', color: 'white', padding: '5px' }}>Register</RouterLink>
+              <RouterLink to="/login" className="top-nav-bar__login" style={{ textDecoration: 'none', color: 'white', padding: '5px', fontFamily: "'Zilla Slab', Arial, sans-serif" }}>Login</RouterLink>
+              <RouterLink to="/register" className="top-nav-bar__register" style={{ textDecoration: 'none', color: 'white', padding: '5px', fontFamily: "'Zilla Slab', Arial, sans-serif" }}>Register</RouterLink>
             </>
           )}
         </div>
