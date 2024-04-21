4845bb76211d74ae801d3b15351a75776fa6d8ff
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-20 19:51:05 -0700

    EDIT: refactor home page with MUI update styling

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,61 +1,71 @@
 import '../styles/HomePage.scss';
 import { Link } from 'react-router-dom';
+import { Grid, Typography, Button } from '@mui/material';
+import FullLogo_Transparent from "../FullLogo_Transparent.png";
 
 const HomePage = () => {
+    return (
+      <Grid container className="home-page" spacing={3} style={{ width: 'auto'}}>
+        <Grid item xs={12} md={6}>
+          <img src={FullLogo_Transparent } alt="Logo" style={{ width: '100%', height: 'auto', paddingTop:'50px' }} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+        <Typography variant="h1" >Who We Are</Typography>
+          <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+            <Typography variant="body1" className="description" style={{ width: '100%', height: 'auto', paddingTop:'50px', marginRight: '150px'}}>
+          Wild Link is a groundbreaking web application designed to harness the power of
+          collective goodwill for the conservation of wildlife.
+          Much like popular crowdfunding platforms, Wild Link
+          provides a seamless interface for users to create
+          and support campaigns dedicated solely to wildlife causes.
+          Whether it's protecting endangered species, preserving
+          vital habitats, or funding research initiatives, Wild Link
+          empowers users to directly contribute to the well-being of our
+          planet's diverse flora and fauna. With its intuitive design and
+          robust features, Wild Link serves as a virtual hub where passionate
+          individuals, organizations, and wildlife enthusiasts unite to make a
+          tangible difference in the conservation efforts worldwide. Join the
+          Wild Link community today and be a part of the movement to
+          safeguard our planet's natural treasures for generations to come.
+          </Typography>
+          </div>
+      </Grid>
 
-  return (
-    <div className="home-page">
-      <img className="home-cover" src="/home-cover-photo.png" alt="home" />
-
-          <h1>Welcome to Wild Link</h1>
-          <p className="description">
-            Wild Link is a groundbreaking web application designed to harness the power of
-            collective goodwill for the conservation of wildlife.
-            Much like popular crowdfunding platforms, Wild Link
-            provides a seamless interface for users to create
-            and support campaigns dedicated solely to wildlife causes.
-            Whether it's protecting endangered species, preserving
-            vital habitats, or funding research initiatives, Wild Link
-            empowers users to directly contribute to the well-being of our
-            planet's diverse flora and fauna. With its intuitive design and
-            robust features, Wild Link serves as a virtual hub where passionate
-            individuals, organizations, and wildlife enthusiasts unite to make a
-            tangible difference in the conservation efforts worldwide. Join the
-            Wild Link community today and be a part of the movement to
-            safeguard our planet's natural treasures for generations to come.
-          </p>
-       
+      <Grid item xs={12} md={6}>
+        <Typography variant="h2">Support Wildlife Rehabilitation</Typography>
+        <Typography variant="body1" className="support-wildlife" style={{ width: '100%', height: 'auto', paddingTop:'50px', paddingBottom: '50px' }}>
+          Supporting local wildlife rehabilitation efforts is vital for preserving 
+          biodiversity and maintaining ecological balance. By assisting in the care
+          of injured or orphaned wildlife, individuals contribute directly to the
+          health of their local ecosystems. Moreover, these initiatives promote
+          environmental awareness and conservation ethics within the community,
+          fostering a deeper connection to nature.
+        </Typography>
+        <Button variant="contained" color="primary" component={Link} to="/donate" className="home-page__donate">
+          Donate Now
+          </Button>
+      </Grid>
+      <Grid item xs={12} md={6}>
         <img className="home-pic2" src="/home-page-pic2.png" alt="home" />
+      </Grid>
 
-        <div class="content-container">
-  <div class="content-left">
-    <h2>Support Wildlife Rehabilitation</h2>
-    <p class="support-wildlife">
-      Supporting local wildlife rehabilitation efforts is vital for preserving 
-      biodiversity and maintaining ecological balance. By assisting in the care
-      of injured or orphaned wildlife, individuals contribute directly to the
-      health of their local ecosystems. Moreover, these initiatives promote
-      environmental awareness and conservation ethics within the community,
-      fostering a deeper connection to nature.
-    </p>
-    <Link to="/donate" className="home-page__donate">Donate Now</Link>
-  </div>
-  <div class="content-right">
-    <h2>Rehaber Needing Public Support?</h2>
-    <p class='rehab-public-support'>
-      We're excited to offer you the opportunity to utilize our
-      app, Wild Link, for your conservation initiatives.
-      With Wild Link, you can easily create and support
-      campaigns dedicated to wildlife causes. Simply reach
-      out to us, and we'll provide you with the necessary
-      resources to get started. Let's work together to
-      make a tangible difference in wildlife conservation.
-      </p>
-      <h3>Contact Us Today!</h3>
-      </div>
-      </div>
-      </div>
-        
+      <Grid item xs={12} md={6}>
+        <img className="home-cover" src="/home-cover-photo.png" alt="home" />
+      </Grid>
+      <Grid item xs={12} md={6} className="content-right">
+        <Typography variant="h2">Rehaber Needing Public Support?</Typography>
+        <Typography variant="body1" className='rehab-public-support' style={{ width: '100%', height: 'auto', paddingTop:'50px'}}>
+          We're excited to offer you the opportunity to utilize our
+          app, Wild Link, for your conservation initiatives.
+          With Wild Link, you can easily create and support
+          campaigns dedicated to wildlife causes. Simply reach
+          out to us, and we'll provide you with the necessary
+          resources to get started. Let's work together to
+          make a tangible difference in wildlife conservation.
+          </Typography>
+        <Typography variant="h3" style={{ width: '100%', height: 'auto', paddingTop:'50px'}}>Contact Us Today!</Typography>
+      </Grid>
+    </Grid>
   );
 };
 
--- frontend/src/components/TopNavigation.jsx
+++ frontend/src/components/TopNavigation.jsx
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, Typography, Box } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import { useAuth } from "../contexts/AuthContext";
+import FullLogo_Transparent from "../FullLogo_Transparent.png";
 
 const TopNavigation = () => {
   const { user, logout } = useAuth();
@@ -21,7 +22,7 @@ const TopNavigation = () => {
 }}>
        <Toolbar style={{ padding: 0 }}>
        <Typography variant="h6" style={{ flexGrow: 3, fontFamily: "'Luckiest Guy', cursive", fontSize: '38px' }}>
-            <RouterLink to="/home" style={{ textDecoration: 'none', color: 'white' }}>Wild Link</RouterLink>
+       <RouterLink to="/home" style={{ textDecoration: 'none', color: 'white' }}>Wild Link</RouterLink>
           </Typography>
 
         <Box display="flex" justifyContent="flex-start" style={{ padding: '20px' }}>
@@ -58,73 +59,3 @@ const TopNavigation = () => {
 };
 
 export default TopNavigation;
-
-//remove orgId === null from line 47
-
-/*          {loggedIn ? (
-            <Box>
-              <Typography variant="h6">{email}</Typography>
-              {orgId && userType !== 'user' ? (
-                <Button color="inherit"><RouterLink to="/users" style={{ textDecoration: 'none', color: 'white' }}>View Profile</RouterLink></Button>
-              ) : (
-                <Button color="inherit"><RouterLink to={`/organizations/${orgId}/profile`} style={{ textDecoration: 'none', color: 'white' }}>View Profile</RouterLink></Button>
-              )}
-              <Button color="inherit" onClick={handleLogout}>Logout</Button>
-            </Box>
-          ) : (
-            <Box>
-              <Button color="inherit"><RouterLink to="/login" style={{ textDecoration: 'none', color: 'white' }}>Login</RouterLink></Button>
-              <Button color="inherit"><RouterLink to="/register" style={{ textDecoration: 'none', color: 'white' }}>Register</RouterLink></Button>
-            </Box>
-          )}
-          */
-
-
-
-// import { Link } from 'react-router-dom';
-// import { useNavigate } from 'react-router-dom';
-
-// const TopNavigation = ({loggedIn, setLoggedIn, email, userType, orgId}) => {
-//   const navigate = useNavigate();
-//   console.log('TopNavigation:', loggedIn, email);
-
-//   const handleLogout = () => {
-//     setLoggedIn(false);
-//     navigate('/home');
-//   };
-
-//   return (
-//     <div className="top-nav-bar">
-//       <Link to="/home" className="top-nav-bar__logo">Wild Link</Link>
-      
-//       <div className="top-nav-bar__links">
-//         <Link to="/patientstories" className="top-nav-bar__patient-stories">Patient Stories</Link>
-//         <span className="top-nav-bar__about-us">About Us</span>
-//         <Link to="/donate" className="top-nav-bar__donate">Donate</Link>
-//         <Link to="/contactus" className="top-nav-bar__contactus">Contact Us</Link>
-//         </div>
-
-//         <div className="top-nav-bar__login-register">
-
-//         {loggedIn ? (
-//           <>
-//             <span className="top-nav-bar__login-message">{email}</span>
-//             {orgId === null ? (
-//                <Link to="/users" className="top-nav-bar__dashboard">View Profile</Link>
-//             ) : (
-//               <Link to={`/organizations/${orgId}/profile`} className="top-nav-bar__dashboard">View Profile</Link>
-//             )}
-//             <button onClick={handleLogout} className="top-nav-bar__logout">Logout</button>
-//           </>
-//         ) : (
-//           <>
-//             <Link to="/login" className="top-nav-bar__login">Login</Link>
-//             <Link to="/register" className="top-nav-bar__register">Register</Link>
-//           </>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TopNavigation;
