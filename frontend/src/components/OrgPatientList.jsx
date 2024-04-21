b8eca075c6a4461185a2e18e6ba2906aa00d181d
David Carmichael <carmichael.db88@gmail.com>
2024-04-20 18:18:29 -0700

    Fix: org patient list access to org only, Fix: org patient list display message if no patient in tab

--- frontend/src/components/OrgPatientList.jsx
+++ frontend/src/components/OrgPatientList.jsx
@@ -58,74 +58,121 @@ const OrgPatientList = () => {
     }
   }
 
+  if (!user || user.role !== "organizations") {
+    // Redirect them to the home page
+   return <Navigate to="/home" replace />;
+  }
+
   return (
     <Box sx={{ width: "100%" }}>
       <Typography variant="h3" align="left" sx={{ mt: 4, mb: 4, ml: 10 }}>
         Patient List
       </Typography>
-      <Button variant="contained" color="primary" sx={{ mb: 4, ml: 10 }} onClick={() => goToNewForm(orgId)}>
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{ mb: 4, ml: 10 }}
+        onClick={() => goToNewForm(orgId)}
+      >
         Add New Patient
       </Button>
       <Paper sx={{ width: "75%", mx: "auto", mb: 2, boxShadow: 3 }}>
-        <Tabs 
-          value={selectedTab} 
-          onChange={handleChangeTab} 
+        <Tabs
+          value={selectedTab}
+          onChange={handleChangeTab}
           aria-label="Patient Tabs"
-          variant="fullWidth"  // Ensures the tabs are equally spaced
-          sx={{ borderBottom: 1, borderColor: 'divider' }}
+          variant="fullWidth" // Ensures the tabs are equally spaced
+          sx={{ borderBottom: 1, borderColor: "divider" }}
         >
           <Tab label="Current Patients" />
           <Tab label="Released Patients" />
         </Tabs>
-      </Paper>  
+      </Paper>
       <Grid
         container
         spacing={2}
-        sx={{ width: "75%", mx: "auto", boxShadow: 3, p: 3, display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(450px, 1fr))", gap: 2, alignItems: "flex-start", justifyContent: "center"}} >
-        {patients ? (
-          patients.filter(patient => (selectedTab === 0 ? !patient.is_released : patient.is_released)).map((patient, index) => (
-            <Grid
-              item
-              sx={{ width: 450, mb: 2, mx: "auto" }}
-              key={index}
-            >
-              <Card sx={{ width: "100%", maxWidth: 450, display: "flex", flexDirection: "row", alignItems: "center", p: 2, boxShadow: 2, mb: 2 }}>
-                <CardContent sx={{ flexGrow: 1, display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-                  <Typography variant="h6">
-                    {patient.species} - {patient.patient_case}
-                  </Typography>
-                  <Box>
-                    <IconButton
-                      color="primary"
-                      aria-label="edit"
+        sx={{
+          width: "75%",
+          mx: "auto",
+          boxShadow: 3,
+          p: 3,
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(450px, 1fr))",
+          gap: 2,
+          alignItems: "flex-start",
+          justifyContent: "center",
+        }}
+      >
+        {patients && patients.length > 0 ? (
+          patients.filter((patient) =>
+            selectedTab === 0 ? !patient.is_released : patient.is_released
+          ).length > 0 ? (
+            patients
+              .filter((patient) =>
+                selectedTab === 0 ? !patient.is_released : patient.is_released
+              )
+              .map((patient, index) => (
+                <Grid item sx={{ width: 450, mb: 2, mx: "auto" }} key={index}>
+                  <Card
+                    sx={{
+                      width: "100%",
+                      maxWidth: 450,
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      p: 2,
+                      boxShadow: 2,
+                      mb: 2,
+                    }}
+                  >
+                    <CardContent
                       sx={{
-                        mr: 1,
-                        border: "1px solid",
-                        borderColor: "primary.main",
-                        boxShadow: 2,
+                        flexGrow: 1,
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
                       }}
-                      onClick={() => goToEditForm(orgId, patient.id)}
                     >
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton
-                      color="secondary"
-                      aria-label="delete"
-                      sx={{
-                        mr: 1,
-                        border: "1px solid",
-                        borderColor: "primary.main",
-                        boxShadow: 2,
-                      }}
-                      onClick={() => confirmDelete(orgId, patient.id)}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </Box>
-                </CardContent>
-              </Card>
+                      <Typography variant="h6">
+                        {patient.species} - {patient.patient_case}
+                      </Typography>
+                      <Box>
+                        <IconButton
+                          color="primary"
+                          aria-label="edit"
+                          sx={{
+                            mr: 1,
+                            border: "1px solid",
+                            borderColor: "primary.main",
+                            boxShadow: 2,
+                          }}
+                          onClick={() => goToEditForm(orgId, patient.id)}
+                        >
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton
+                          color="secondary"
+                          aria-label="delete"
+                          sx={{
+                            mr: 1,
+                            border: "1px solid",
+                            borderColor: "primary.main",
+                            boxShadow: 2,
+                          }}
+                          onClick={() => confirmDelete(orgId, patient.id)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+          ) : (
+            <Grid item xs={12}>
+              <Typography>No Patients Found in this category.</Typography>
             </Grid>
-          ))
+          )
         ) : (
           <Grid item xs={12}>
             <Typography>No Patients Found, Create a patient!</Typography>
