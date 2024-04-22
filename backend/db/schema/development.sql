7bdc7f896756668ce1eab7b0a67e9da3b3554b22
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-21 22:31:05 -0700

    ADD reseed with more data

--- backend/db/schema/development.sql
+++ backend/db/schema/development.sql
@@ -1,13 +1,37 @@
 INSERT INTO organizations (organization_name, website_url, image, first_name, last_name, phone_number, email, address, password, is_deleted, created_at, updated_at)
 VALUES ('MARS Wildlife Rescue Centre', 'www.marswildliferescue.com', 'https://marswildliferescue.com/wp-content/uploads/2017/08/MARS-LOGO-transparent-background.png', 'Gyl', 'Andersen', '250-337-2021', 'gyl@marswildliferescue.com', '1331 Williams Beach Road,
-Merville, British Columbia', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+Merville, British Columbia', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Ocean Conservation Society', 'www.oceanconservationsociety.org', '/oceanOrg.jpg', 'Avery', 'Chang', '555-123-4567', 'avery@oceanconservationsociety.org', '123 Ocean Ave, Seaville, CA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ ('Global Health Initiative', 'www.globalhealthinitiative.org', '-----', 'Ethan', 'Tran', '555-234-5678', 'ethan@globalhealthinitiative.org', '456 Health Blvd, Wellville, NY', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Clean Energy Alliance', 'www.cleanenergyalliance.org', '-----', 'Olivia', 'Kumar', '555-345-6789', 'olivia@cleanenergyalliance.org', '789 Renewable St, Greentown, OR', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Wildlife Conservation Foundation', 'www.wildlifeconservationfoundation.org', '-----', 'Liam', 'Nguyen', '555-456-7890', 'liam@wildlifeconservationfoundation.org', '101 Forest Ave, Wildville, WA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Community Food Bank', 'www.communityfoodbank.org', '-----', 'Harper', 'Patel', '555-567-8901', 'harper@communityfoodbank.org', '202 Harvest Lane, Foodville, TX', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Green Earth Alliance', 'www.greenearthalliance.org', '-----', 'Noah', 'Garcia', '555-678-9012', 'noah@greenearthalliance.org', '303 Eco Blvd, Sustainaville, NV', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Tech for Good Foundation', 'www.techforgoodfoundation.org', '-----', 'Ava', 'Wong', '555-789-0123', 'ava@techforgoodfoundation.org', '404 Innovation Ave, Techville, CA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Animal Rescue Coalition', 'www.animalrescuecoalition.org', '-----', 'Mia', 'Johnson', '555-901-2345', 'mia@animalrescuecoalition.org', '606 Pet Blvd, Petville, CA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Climate Action Network', 'www.climateactionnetwork.org', '-----', 'Logan', 'Martinez', '555-012-3456', 'logan@climateactionnetwork.org', '707 Earth St, Greenworld, NY', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Arts for All Foundation', 'www.artsforallfoundation.org', '-----', 'Sophia', 'Brown', '555-123-4567', 'sophia@artsforallfoundation.org', '808 Creativity Blvd, Artsville, TX', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Sports for Everyone Organization', 'www.sportsforeveryone.org', '-----', 'Jackson', 'White', '555-234-5678', 'jackson@sportsforeveryone.org', '909 Athlete Ave, Sportstown, CA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
 INSERT INTO users (first_name, last_name, email, address, password, created_at, updated_at)
 VALUES 
   ('Desiree', 'Ingram', 'desiree@email.com', 'Somewhere, Victoria, BC', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   ('David', 'Carmichael', 'david@email.com', 'Somewhere, Campbell River, BC', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  ('Paul', 'Formby', 'paul@email.com', 'Somewhere, Victoria', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+  ('Paul', 'Formby', 'paul@email.com', 'Somewhere, Victoria', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Emily', 'Parker', 'emily@email.com', '123 Main St, Anytown, USA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Michael', 'Johnson', 'michael@email.com', '456 Elm St, Sometown, CA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Jennifer', 'Garcia', 'jennifer@email.com', '789 Oak St, Anycity, TX', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Daniel', 'Martinez', 'daniel@email.com', '101 Pine St, Anothercity, NY', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Sarah', 'Hernandez', 'sarah@email.com', '202 Maple St, Yetanothercity, FL', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Matthew', 'Lopez', 'matthew@email.com', '303 Cedar St, Anytown, USA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Jessica', 'Gonzalez', 'jessica@email.com', '404 Birch St, Sometown, CA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Christopher', 'Rodriguez', 'christopher@email.com', '505 Fir St, Anycity, TX', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Amanda', 'Wilson', 'amanda@email.com', '606 Spruce St, Anothercity, NY', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('James', 'Martinez', 'james@email.com', '707 Cedar St, Yetanothercity, FL', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Megan', 'Garcia', 'megan@email.com', '808 Oak St, Anytown, USA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Brandon', 'Lopez', 'brandon@email.com', '909 Pine St, Sometown, CA', '$2b$10$8mG8IaIVhP2NNdkzr1mQTO6nSLfQ2QJFbsCdncmL3Mm5gKEDeTIhy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+  
 
 
 INSERT INTO species (name, scientific_name, description, image, created_at, updated_at)
@@ -15,7 +39,19 @@ VALUES
   ('Bald Eagle', 'Haliaeetus leucocephalus', 'The bald eagle is a bird of prey found in North America. A sea eagle, it has two known subspecies and forms a species pair with the white-tailed eagle, which occupies the same niche as the bald eagle in the Palearctic.', 'bald-eagle.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   ('Common Raven', 'Corvus corax', 'The common raven is a large all-black passerine bird. It is the most widely distributed of all corvids, found across the Northern Hemisphere.', 'common-raven.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   ('Raccoon', 'Procyon lotor', 'Raccoons are stocky animals with short legs and small, rounded ears. Their fur is gray, with dark black markings around their eyes, and black bands on their tail.', 'raccoon.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  ('Varied Thrush', 'Ixoreus naevius', 'The varied thrush is a member of the thrush family, Turdidae. It is the only species in the monotypic genus Ixoreus.', 'varied-thrush.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+  ('Varied Thrush', 'Ixoreus naevius', 'The varied thrush is a member of the thrush family, Turdidae. It is the only species in the monotypic genus Ixoreus.', 'varied-thrush.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Sea Otter', 'Enhydra lutris', 'The sea otter is a marine mammal native to the coasts of the northern and eastern North Pacific Ocean. They are known for their playful behavior and use of tools, such as rocks, to crack open shellfish.', 'sea-otter.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('River Otter', 'Lontra canadensis', 'The river otter is a semiaquatic mammal found in North America. They are skilled swimmers and feed on fish, crustaceans, and other aquatic prey.', 'river-otter.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Seal', 'Pinniped', 'Seals are marine mammals characterized by their streamlined bodies, flippers, and ability to swim gracefully underwater. They are found in both polar and temperate waters.', 'seal.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Sea Lion', 'Otariinae', 'Sea lions are marine mammals belonging to the family Otariidae. They are characterized by external ear flaps, long foreflippers, and the ability to walk on land using all four limbs.', 'sea-lion.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Marmot', 'Marmota', 'Marmots are large ground squirrels belonging to the genus Marmota. They are typically found in mountainous regions and are known for their burrowing behavior and loud warning calls.', 'marmot.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Roosevelt Elk', 'Cervus canadensis roosevelti', 'The Roosevelt elk is the largest of the four surviving subspecies of elk in North America. They are primarily found in the Pacific Northwest region.', 'roosevelt-elk.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Crow', 'Corvus', 'Crows are highly intelligent birds belonging to the genus Corvus. They are known for their adaptability, problem-solving skills, and distinct cawing calls.', 'crow.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Woodpecker', 'Picidae', 'Woodpeckers are a family of birds known for their drumming behavior on trees and ability to excavate wood to find insects. They have strong bills and specialized tongues.', 'woodpecker.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Swallow', 'Hirundinidae', 'Swallows are small passerine birds known for their graceful aerial acrobatics. They feed on insects caught in flight and often build mud nests.', 'swallow.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Sandpiper', 'Scolopacidae', 'Sandpipers are a diverse family of shorebirds found worldwide. They have long bills for probing in mud and sand, and they feed on small invertebrates.', 'sandpiper.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Deer', 'Cervidae', 'Deer are hoofed mammals belonging to the family Cervidae. They are known for their antlers, which are typically found on males, and their herbivorous diet.', 'deer.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Moose', 'Alces alces', 'The moose, also known as elk in Europe, is the largest extant species in the deer family. They are primarily found in boreal and mixed deciduous forests.', 'moose.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
 INSERT INTO conditions (condition_name, created_at, updated_at)
@@ -26,6 +62,7 @@ VALUES
   ('Infection', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   ('Emaciation', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   ('Window Strike', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('Boat Strike', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   ('Car Strike', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   ('Head Trauma', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
@@ -94,4 +131,10 @@ INSERT INTO donations (user_id, organization_id, patient_id, donation_in_cents, 
 VALUES
   (1, 1, 1, 10000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   (2, 1, 3, 5000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (3, 1, 2, 2500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+  (3, 1, 2, 2500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (3, 1, 2, 3500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (4, 1, 1, 600000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (2, 2, 2, 2500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (1, 1, 4, 4500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+
