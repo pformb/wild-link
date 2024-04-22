21b2bce2b5ecefc7f856e9ebf631024572a9c6bb
Kershia1 <insatiable.meerkat@gmail.com>
2024-04-21 17:45:58 -0700

    EDIT: remove default MUI theme

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -11,16 +11,14 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from "jwt-decode";
 import { useAuth } from "../contexts/AuthContext";
 
-const defaultTheme = createTheme();
-
 const LoginPage = () => {
+  // const theme = useTheme();
   const navigate = useNavigate();
 
   const [email, setEmail] = useState('');
@@ -28,16 +26,25 @@ const LoginPage = () => {
   const [open, setOpen] = useState(false);
   const { setAuthData } = useAuth();
 
+  // const useStyles = makeStyles({
+  //   root: {
+  //     color: theme.palette.primary.main,
+  //     fontFamily: theme.typography.fontFamily,
+  //     // Add more styles as needed
+  //   },
+  //   // Add more classes as needed
+  // });
 
-  const handleSubmit = async (e) => { 
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const loginData = {
       email,
       password
     };
-  
+
     try {
-      console.log("login data is:", loginData)
+      console.log("login data is:", loginData);
       const response = await fetch('http://localhost:3001/api/login', {
         method: 'POST',
         headers: {
@@ -46,14 +53,14 @@ const LoginPage = () => {
         body: JSON.stringify(loginData),
         credentials: 'include'
       });
-  
+
       const data = await response.json();
-  
+
       if (response.ok && data.success) {
         // Login successful
         localStorage.setItem("token", data.token); // Save the token in localStorage
         setAuthData(data.token);
-  
+
         navigate('/home');
       } else {
         // Login failed
@@ -66,69 +73,67 @@ const LoginPage = () => {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Login
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
                 fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Login
-              </Button>
-            </Box>
-          </Box>
-        <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+      <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
         <Alert onClose={() => setOpen(false)} severity="error" sx={{ width: '100%' }}>
           Invalid email or password. Please try again.
         </Alert>
-    </Snackbar>
-      </Container>
-    </ThemeProvider>
+      </Snackbar>
+    </Container>
   );
-}
+};
 
 export default LoginPage;
